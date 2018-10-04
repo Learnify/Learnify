c4c81398d5d21409b3243070d15714755d89ac01
nviverosb <nviverosb@unal.edu.co>
2018-10-03 22:30:16 -0500

    first commit

---
+++ src/components/landing/Navbar.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Label,
+  Button,
+  Input,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  FormGroup
+} from "reactstrap";
+import "../../styles/landing/navbar.css";
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  state = {};
+  render() {
+    return (
+      <Navbar light expand="md">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-2" align="left">
+              <NavbarBrand className="learnify-navbar all" href="/">
+                Learnify
+              </NavbarBrand>
+            </div>
+            <div className="col-lg-6">
+              <FormGroup className="search-container w-100 md-0 active-cyan-3 active-cyan-4">
+                <Input
+                  type="search"
+                  name="search"
+                  id="exampleSearch"
+                  placeholder="search for tutoring..."
+                />
+              </FormGroup>
+            </div>
+            <div className="col-lg-4">
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto navbar-inverse" navbar>
+                  <NavItem>
+                    <NavLink className="all" href="/components/">
+                      Contact
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <div className="navbar-blog">
+                      <NavLink className="all" href="/components/">
+                        Blog
+                      </NavLink>
+                    </div>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/components/" className="auth-button">
+                      <Button color="info" outline>
+                        Login
+                      </Button>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/components/" className="auth-button">
+                      <Button color="info" outline>
+                        Sign Up
+                      </Button>
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </div>
+          </div>
+        </div>
+      </Navbar>
+    );
+  }
+}
+
+export default NavBar;
