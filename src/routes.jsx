112c97b2536cc8319f747b66f7f2b7741d35e8bd
Brandon Sneyder Avilan Rodriguez <BrandonAvilan@MacBook-Pro-de-Brandon.local>
2018-10-18 23:00:57 -0500

    Protected routes V0

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,11 @@
 //Depencencies
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, {Component} from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 //Components
 import App from "./components/App";
 import Landing from "./components/landing/Lading";
-import Profile from "./components/profile/Profile";
+// import Profile from "./components/profile/Profile";
 import Page404 from "./components/Page404";
 import SignInForm from "./components/authentication/SignInForm";
 import SignUpForm from "./components/authentication/SignUpForm";
@@ -13,16 +13,25 @@ import Authentication from "./components/authentication/authentication";
 import Contact from "./components/contact/contact";
 import Blog from "./components/blog/blog";
 
+// Muchachos lo dejo comentado para que no genere errores al ejecutar
+// const PrivateRoute = ({ component: Component, ...rest}) => (
+//   <Route {...rest} render ={(props)=>(
+//     fakeAuth.isAuthenticated === true Este es el condicional que toca modificar cuando se implemente la sesion omitan esto si no lo entienden
+//       ? <Component {...props}/>
+//       : <Redirect to ='/LogIn'/>
+//   )}/>
+// )
+
 const AppRouters = () => (
   <App>
     <Switch>
-      {/* <Route exact path='/Profile' component={Profile}/> */}
       <Route exact path="/Authentication" component={Authentication} />
       <Route exact path="/LogIn" component={SignInForm} />
       <Route exact path="/SignIn" component={SignUpForm} />
       <Route exact path="/Contact" component={Contact} />
       <Route exact path="/Blog" component={Blog} />
       <Route exact path="/" component={Landing} />
+      {/* <PrivateRoute exact path='/Profile' component={Profile}/>   */}
       <Route component={Page404} />
     </Switch>
   </App>
