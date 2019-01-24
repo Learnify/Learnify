6c5f53360260ce6bdf61bbea0c610c2ff9bcb912
Daniel <dcastanedar@unal.edu.co>
2019-01-24 09:07:38 -0500

    Footer positioned and first version of landing page

--- src/components/landing/Lading.jsx
+++ src/components/landing/Lading.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-// import NavBar from "./Navbar.jsx";
+import "./Landing.css";
+//import NavBar from "./Navbar.jsx";
 
 class Landing extends Component {
   render() {
     return (
-      <div className="landing">
-        <h1>Landing page</h1>
+      <div className="container landing">
+        <h1>
+          LEARNIFY
+        </h1>
+        <p class="main" align="center">
+        Welcome to the app that will help you improve all your skills.
+        </p>
       </div>
     );
   }
