f2f19f5ce9c5e041f94819e7e025d76f472d1b5b
BrandonAvilanR <bsavilanr@unal.edu.co>
2019-03-10 22:31:45 -0500

    chat view 100% but not working

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './chat.css';
 
+import { connect } from "react-redux";
+
 class Chat extends Component {
+
     render() {
         return (
             <div>
@@ -12,10 +15,17 @@ class Chat extends Component {
                         </div>
                         <div className="chatContent" id="left">
                             <div className="conversationSearch">
-                                <input type="search" className="conversationSearchInput" placeholder="Search Messages"/>
+                                <input type="search" className="conversationSearchInput" placeholder="Search Messages" />
                             </div>
                             <div className="conversationListItem">
-                                <img class="conversationPhoto" src="https://randomuser.me/api/portraits/men/73.jpg" alt="conversation"/>
+                                <img class="conversationPhoto" src="https://randomuser.me/api/portraits/men/73.jpg" alt="conversation" />
+                                <div className="conversationInfo">
+                                    <h1 className="conversationTitle">some peole</h1>
+                                    <p className="conversationSnippet"> Hello world! This is a long message that</p>
+                                </div>
+                            </div>
+                            <div className="conversationListItem">
+                                <img class="conversationPhoto" src="https://randomuser.me/api/portraits/men/73.jpg" alt="conversation" />
                                 <div className="conversationInfo">
                                     <h1 className="conversationTitle">some peole</h1>
                                     <p className="conversationSnippet"> Hello world! This is a long message that</p>
@@ -28,10 +38,16 @@ class Chat extends Component {
                             <h1>Conversation Title</h1>
                         </div>
                         <div className="chatContent" id="right">
-                            somethisn
-
+                            <div className="messageListContainer">
+                                <div className="time">Sunday, March 10, 2019 9:59 PM</div>
+                            </div>
+                            <div className="bubbleContainer">
+                                <div className="bubble">
+                                    Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.
+                                </div>
+                            </div>
                             <div className="chatText">
-                                <input type="text" className="composeInput" placeholder="Type a message"/>
+                                <input type="text" className="composeInput" placeholder="Type a message" />
                                 <button type="submit" className="chatSend icon-paper-plane-empty"></button>
                             </div>
                         </div>
@@ -42,4 +58,14 @@ class Chat extends Component {
         );
     }
 }
-export default Chat;
+
+
+function mapStateToProps(state) {
+    const { loggedIn } = state.authentication;
+    return {
+        loggedIn
+    };
+}
+
+const connectedChat = connect(mapStateToProps)(Chat);
+export default connectedChat;
